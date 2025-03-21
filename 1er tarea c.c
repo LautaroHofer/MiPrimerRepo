3ecf3f4e525327332e6e97f1e874ec0fabe81346
Lautaro Hofer <lautarohofer@gmail.com>
2025-03-21 15:07:38 -0300

    Add files via upload

---
+++ 1er tarea c.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, temp;
+	
+	printf("Ingrese tres numeros separados por espacios: ");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if (a > b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (b > c) {
+		temp = b;
+		b = c;
+		c = temp;
+	}
+	if (a > b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	
+	printf("Numeros ordenados de menor a mayor: %d %d %d\n", a, b, c);
+	
+	return 0;
+}
+
